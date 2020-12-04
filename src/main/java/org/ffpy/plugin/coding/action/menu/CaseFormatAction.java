4c33a469013659a039ad52612ee9983c623d9623
ffpy <q1411603774@163.com>
2020-12-04 10:10:34 +0800

    1. 添加驼峰-下划线互转功能 2. 添加SQL字段用反引号包裹功能

---
+++ src/main/java/org/ffpy/plugin/coding/action/menu/CaseFormatAction.java
@@ -0,0 +1,19 @@
+package org.ffpy.plugin.coding.action.menu;
+
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import org.ffpy.plugin.coding.action.ActionService;
+import org.ffpy.plugin.coding.action.BaseReplaceAction;
+import org.ffpy.plugin.coding.util.MyStringUtils;
+
+/**
+ * 驼峰-下划线互转
+ */
+@ActionService
+public class CaseFormatAction extends BaseReplaceAction {
+
+    @Override
+    protected String replace(AnActionEvent e, String text) throws Exception {
+        return text.contains("_") ? MyStringUtils.underScoreCase2CamelCase(text) :
+                MyStringUtils.camelCase2UnderScoreCase(text);
+    }
+}
